2e40e455082a7a1e0ea6b878dd60aed4cd64a03e
gabriel gonçalves <ggr0910@hotmail.com>
2026-01-29 13:25:33 -0300

    Base

---
+++ MVC_Template/Controllers/BaseController.cs
@@ -0,0 +1,41 @@
+﻿using Application.Application;
+using Application.Interface;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Security.Claims;
+
+namespace LACS_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BaseController : Controller
+    {
+        protected IUnitOfWorkApplication _application;
+        protected IConfiguration _configuration;
+
+        public BaseController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _application = new UnitOfWorkApplication(configuration);
+        }
+
+        [ApiExplorerSettings(IgnoreApi = true)]
+        public  void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (context.ActionDescriptor.EndpointMetadata.OfType<AllowAnonymousAttribute>().Any())
+                return;
+
+            var authorizationHeader = context.HttpContext.Request.Headers["Authorization"].FirstOrDefault();
+            if (string.IsNullOrEmpty(authorizationHeader))
+            {
+                context.Result = new UnauthorizedResult();
+                return;
+            }
+            else
+            {
+               
+            }
+        }
+    }
+}
